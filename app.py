f88ed18fed6f18403379ae9313a786e43b75dc34
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-11 12:02:36 -0600

    stub initial routes

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/web/audio", methods = ['GET', 'POST'])
+def audio_index():
+    return "Audio created!"
+
+@app.route("/web/audio/<ID>", methods = ['GET'])
+def get_audio(ID):
+    return "Audio {ID} retrieved!"
+
+@app.route("/files/audio", methods = ['GET', 'POST'])
+def audio_index():
+    return "Audio created!"
+
+@app.route("/files/audio/<ID>", methods = ['GET'])
+def get_audio(ID):
+    return "Audio {ID} retrieved!"
