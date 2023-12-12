4e5e9c9d9e4228d4b1612183d2b4c677b9092922
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-12 15:38:52 -0600

    set up endpoint to get generated audio clips

--- app/routes/audio.py
+++ app/routes/audio.py
@@ -1,13 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from app.models.user import User
 
 audio_bp = Blueprint('audio', __name__)
 
-@audio_bp.get('/')
-@jwt_required
+@audio_bp.get('/audio_clips')
+@jwt_required()
 def index():
   current_user_id = get_jwt_identity();
-
-  User.query.get(id=current_user_id).first().audio_clips()
+  print(User.query.get(current_user_id).audio_clips())
+  return jsonify(audio_clips=User.query.get(current_user_id).audio_clips())
