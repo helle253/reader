4b75f398dbfef814d4522a48520dcddcf15c5510
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-12 15:52:27 -0600

    rename file

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.models.audio_clip import AudioClip;
 
-from app.persistence.connector import open_connection;
+from app.persistence.psycopg_helpers import open_connection;
 from . import db;
 
 class User(db.Model):
