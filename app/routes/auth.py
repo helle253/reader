6ffa18e5548455069c6a88548f4cf1823fc4ddf9
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-12 14:53:34 -0600

    parse request body from POST requests

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,3 +1,4 @@
+import json
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token
 from werkzeug.security import check_password_hash;
@@ -8,21 +9,20 @@ auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.post('/login')
 def login():
-  json = request.get_json();
-  email = json.get("email", None)
-  password = json.get("password", None)
+  parsed = json.loads(request.data);
+  email = parsed["email"] or None
+  password = parsed["password"] or None
   user = User.query.filter_by(email=email).first()
   if user and check_password_hash(user.password_digest, password):
-    token = create_access_token(identity=user.id)
-    return jsonify(status=200, access_token=token)
+    return jsonify(status=200, access_token=create_access_token(identity=user.id))
   else:
     return jsonify(status=422, error='Incorrect email or password')
 
 @auth_bp.post('/register')
 def register():
-  json = request.get_json();
-  email = json.get("email", None)
-  password = json.get("password", None)
+  parsed = json.loads(request.data);
+  email = parsed["email"] or None
+  password = parsed["password"] or None
   existing_user = User.query.filter_by(email=email).first()
   if not existing_user:
     user = User.create(email, password)
