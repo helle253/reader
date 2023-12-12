01f7a43eb7dc10e3399dd26c4d6e20f6c7c7907e
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-12 15:45:53 -0600

    create an endpoint for refreshing tokens

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,6 @@
 import json
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 from werkzeug.security import check_password_hash;
 
 from app.models.user import User
@@ -14,7 +14,7 @@ def login():
   password = parsed["password"] or None
   user = User.query.filter_by(email=email).first()
   if user and check_password_hash(user.password_digest, password):
-    return jsonify(status=200, access_token=create_access_token(identity=user.id))
+    return create_tokens_response(user.id)
   else:
     return jsonify(status=422, error='Incorrect email or password')
 
@@ -26,7 +26,19 @@ def register():
   existing_user = User.query.filter_by(email=email).first()
   if not existing_user:
     user = User.create(email, password)
-    token = create_access_token(identity=user.id)
-    return jsonify(status=200, access_token=token)
+    return create_tokens_response(user.id)
   else:
     return jsonify(status=422, error='User already exists')
+
+@auth_bp.post('/refresh')
+@jwt_required(refresh=True)
+def refresh():
+  identity = get_jwt_identity()
+  return create_tokens_response(identity)
+
+def create_tokens_response(identity):
+  return jsonify(
+    status=200,
+    access_token=create_access_token(identity=identity),
+    refresh_token=create_refresh_token(identity=identity),
+  )
