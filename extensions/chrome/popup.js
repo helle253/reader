0b45354767dd6c722bfaae0e5c4f3e7a61589e2b
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-13 13:32:46 -0600

    add super simple login button (no actual inputs yet

--- extensions/chrome/popup.js
+++ extensions/chrome/popup.js
@@ -1,5 +1,31 @@
+document.addEventListener("DOMContentLoaded", loadState);
+
 document.getElementById("clickMe").addEventListener("click", myFunction);
+document.getElementById("login").addEventListener("click", login);
+
+let host = "http://localhost:5000"
+var tokenPair;
+
+async function loadState() {
+  const cookie = await chrome.cookies.get({url: host, name: "tokenPair"});
+  tokenPair = JSON.parse(cookie.value || '{}');
+}
 
 function myFunction(){
   console.log('clicked!');
+}
+
+async function login(email, password){
+  const resp = await fetch(host + "/auth/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  });
+  tokenPair = (await resp.json())["data"]
+  chrome.cookies.set({url: host, name: "tokenPair", value: JSON.stringify(tokenPair)})
 }
