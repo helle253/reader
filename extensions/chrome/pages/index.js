714e590b7dfc26e046b9b61acaa2bc6a4ef702da
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-13 14:19:38 -0600

    initialize UI

--- extensions/chrome/pages/index.js
+++ extensions/chrome/pages/index.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", loadState);
 
-document.getElementById("clickMe").addEventListener("click", myFunction);
-
 let host = "http://localhost:5000"
 var tokenPair;
 
 async function loadState() {
   const cookie = await chrome.cookies.get({url: host, name: "tokenPair"});
-  tokenPair = JSON.parse(cookie.value || '{}');
+  if (cookie && cookie.value) {
+    try {
+      tokenPair = JSON.parse(cookie.value);
+    } catch (e) {
+      console.error("Error parsing tokenPair cookie.");
+    }
+  }
+
+  initializeUI(!!tokenPair)
 }
 
-function myFunction(){
-  console.log('clicked!');
+function initializeUI(isLoggedIn) {
+  if (isLoggedIn) {
+    document.getElementById("logout").removeAttribute("hidden");
+  } else {
+    document.getElementById("login").removeAttribute("hidden");
+    document.getElementById("register").removeAttribute("hidden");
+  }
 }
+
