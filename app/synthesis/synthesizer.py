1bdb36a657b7dbc0807a439763275be87418a0a0
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2023-12-18 14:11:59 -0600

    strip multiple newlines when needed

--- app/synthesis/synthesizer.py
+++ app/synthesis/synthesizer.py
@@ -1,3 +1,4 @@
+import re
 import os
 from typing import Iterable
 from pyht import Client, TTSOptions
@@ -11,8 +12,15 @@ class Synthesizer:
     self.options = options or TTSOptions(voice=os.getenv("VOICE_URI"))
 
   def synthesize(self, text) -> Iterable[bytes]:
+    preprocessed_text = self.preprocess(text)
     for chunk in self.client.tts(
-      text,
+      preprocessed_text,
       self.options,
     ):
       yield chunk
+
+
+  def preprocess(self, text):
+    # Remove multiple concurrent newlines.
+    text = re.sub(r"\n{1,}", "\n", text)
+    return text
