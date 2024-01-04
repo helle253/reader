ebff07bedd9c1f8d97438cab539c9d835cd7c11c
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2024-01-04 15:35:30 -0600

    whitespace

--- app/synthesis/text_processing.py
+++ app/synthesis/text_processing.py
@@ -4,6 +4,5 @@ from typing import Iterable
 def process_text_selection(text) -> Iterable[str]:
   return to_paragraphs(text)
 
-
 def to_paragraphs(text) -> Iterable[str]:
   return [substr for substr in re.split(r'\n{1,}', text) if substr]
